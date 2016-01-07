66b07255b76cca418f3ab4444ac6232b0852cd0f
Frank Milburn <fmilburn3@users.noreply.github.com>
2016-01-07 13:03:16 -0800

    Create accounts.h

---
+++ accounts.h
@@ -0,0 +1,5 @@
+#define WIFI_SSID       "yourSSID"               // your network name also called SSID
+#define WIFI_PASSWORD   "yourPW"                 // your network password
+
+#define SERVER_IP       "5.9.207.224"            // IP address for MarineTraffic
+#define SERVER_PORT     xxxx                     // your MarineTraffic port
